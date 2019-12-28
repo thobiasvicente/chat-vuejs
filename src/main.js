f956e2534a15dcf5a4bb59a81c8e76082e6b4c57
thobiasvicente <thobiascontact@gmail.com>
2019-12-28 16:17:39 -0300

    index js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,30 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase'
+
 Vue.config.productionTip = false
+
+// Required for side-effects
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC1kkPaDuws2I7Jy0g7NtXF87Ozaf6Gy7o",
+  authDomain: "chat-vuejs-e8a8a.firebaseapp.com",
+  databaseURL: "https://chat-vuejs-e8a8a.firebaseio.com",
+  projectId: "chat-vuejs-e8a8a",
+  storageBucket: "chat-vuejs-e8a8a.appspot.com",
+  messagingSenderId: "148338809674",
+  appId: "1:148338809674:web:e0b82c6979dcba0572ffb3",
+  measurementId: "G-LE0DG512Q9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var db = firebase.firestore();
+
+window.db = db;
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import PrivateChat from '../views/PrivateChat.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +10,12 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: PrivateChat
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   },
   {
     path: '/about',
@@ -17,7 +24,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
 ]
 
 const router = new VueRouter({
